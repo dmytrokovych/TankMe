8dae313c65a2f51530d26065d14c6a2fbd2f97d6
dmytrokovych <dmytrokovych@gmail.com>
2022-01-30 22:42:59 +0100

    screen split

--- level.py
+++ level.py
@@ -11,9 +11,6 @@ from random import choice
 class Level:
     def __init__(self):
 
-        # get the display surface (from anywhere)
-        # self.display_surface = pygame.display.get_surface()
-
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
         self.obstacle_sprites = pygame.sprite.Group()
@@ -50,21 +47,23 @@ class Level:
                             Tile((x, y), [self.visible_sprites,
                                  self.obstacle_sprites], 'object', surf)
 
-        self.player = Player(
+        self.player_left = Player(
             (2000, 1430), [self.visible_sprites], self.obstacle_sprites)
-
+        self.player_right = Player(
+            (2100, 1430), [self.visible_sprites], self.obstacle_sprites)
 
     def run(self):
         # update and draw the game
 
-        self.bullets = self.player.get_bullets()
+        self.bullets = self.player_left.get_bullets()
         self.visible_sprites.add(self.bullets)
 
         for bullet in self.bullets:
             if pygame.sprite.spritecollide(bullet, self.obstacle_sprites, False):
                 bullet.kill()
 
-        self.visible_sprites.custom_draw(self.player)
+        self.visible_sprites.custom_draw(self.player_left, pos='left')
+        self.visible_sprites.custom_draw(self.player_right, pos='right')
         self.visible_sprites.update()
 
 
@@ -73,10 +72,8 @@ class YSortCameraGroup(pygame.sprite.Group):
 
         # general setup
         super().__init__()
+        # get the display surface (from anywhere)
         self.display_surface = pygame.display.get_surface()
-        self.display1_surface = pygame.Surface((WIDTH//2, HEIGTH)) # TEST
-        self.half_width = self.display1_surface.get_size()[0] // 2
-        self.half_height = self.display1_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
         # creating the floor
@@ -84,20 +81,27 @@ class YSortCameraGroup(pygame.sprite.Group):
             'graphics/tilemap/ground.png').convert()
         self.floor_rect = self.floor_surf.get_rect(topleft=(0, 0))
 
-    def custom_draw(self, player):
+    def custom_draw(self, player_left, pos='left'):
+
+        self.display_surface_half= pygame.Surface((WIDTH//2, HEIGTH))
+
+        self.half_width = WIDTH // 4
+        self.half_height = HEIGTH // 2
 
         # getting the offset
-        self.offset.x = player.rect.centerx - self.half_width
-        self.offset.y = player.rect.centery - self.half_height
+        self.offset.x = player_left.rect.centerx - self.half_width
+        self.offset.y = player_left.rect.centery - self.half_height
 
         # drawing the floor
         floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display1_surface.blit(self.floor_surf, floor_offset_pos)
-        self.display_surface.blit(self.display1_surface, (0,0))
+        self.display_surface_half.blit(self.floor_surf, floor_offset_pos)
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
-            self.display1_surface.blit(sprite.image, offset_pos)
-            self.display_surface.blit(self.display1_surface, (0,0))
-
+            self.display_surface_half.blit(sprite.image, offset_pos)
+        
+        if pos == 'left':
+            self.display_surface.blit(self.display_surface_half, (0, 0))
+        elif pos == 'right':
+         self.display_surface.blit(self.display_surface_half, (WIDTH // 2, 0))
