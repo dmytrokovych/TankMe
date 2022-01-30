06322471e6fb2bc1494978b56556042611e2ebd1
dmytrokovych <dmytrokovych@gmail.com>
2022-01-30 22:04:14 +0100

    start of local multiplayer branch

--- level.py
+++ level.py
@@ -12,7 +12,7 @@ class Level:
     def __init__(self):
 
         # get the display surface (from anywhere)
-        self.display_surface = pygame.display.get_surface()
+        # self.display_surface = pygame.display.get_surface()
 
         # sprite group setup
         self.visible_sprites = YSortCameraGroup()
@@ -74,8 +74,9 @@ class YSortCameraGroup(pygame.sprite.Group):
         # general setup
         super().__init__()
         self.display_surface = pygame.display.get_surface()
-        self.half_width = self.display_surface.get_size()[0] // 2
-        self.half_height = self.display_surface.get_size()[1] // 2
+        self.display1_surface = pygame.Surface((WIDTH//2, HEIGTH)) # TEST
+        self.half_width = self.display1_surface.get_size()[0] // 2
+        self.half_height = self.display1_surface.get_size()[1] // 2
         self.offset = pygame.math.Vector2()
 
         # creating the floor
@@ -91,9 +92,12 @@ class YSortCameraGroup(pygame.sprite.Group):
 
         # drawing the floor
         floor_offset_pos = self.floor_rect.topleft - self.offset
-        self.display_surface.blit(self.floor_surf, floor_offset_pos)
+        self.display1_surface.blit(self.floor_surf, floor_offset_pos)
+        self.display_surface.blit(self.display1_surface, (0,0))
 
         # for sprite in self.sprites():
         for sprite in sorted(self.sprites(), key=lambda sprite: sprite.rect.centery):
             offset_pos = sprite.rect.topleft - self.offset
-            self.display_surface.blit(sprite.image, offset_pos)
+            self.display1_surface.blit(sprite.image, offset_pos)
+            self.display_surface.blit(self.display1_surface, (0,0))
+
