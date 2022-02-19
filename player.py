0b1448ecb9741e94ef6d2f6091998513204682b2
dmytrokovych <dmytrokovych@gmail.com>
2022-02-19 12:30:54 +0100

    map update

--- player.py
+++ player.py
@@ -9,7 +9,7 @@ class Player(pygame.sprite.Sprite):
     def __init__(self, pos, groups, obstacle_sprites):
         super().__init__(groups)
         self.image = pygame.image.load(
-            'graphics/test/player.png').convert_alpha()
+            'graphics/player_left/up/up_0.png').convert_alpha()
         self.rect = self.image.get_rect(topleft=pos)
         self.hitbox = self.rect.inflate(0, -26)
 
